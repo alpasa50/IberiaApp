b3fe41f4fb23e3c23a8184dd590d995f7eee8b33
VillacampaST <villacampast@VillacampaSTs-Mac-mini.local>
2021-08-19 16:48:16 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+//Las librerias material son todos los componentes (Widgets) de IOS Y Android, son compatibles
+//Cupertino solamente son componentes (Widgets) de IOS. 
+import 'package:flutter/material.dart';
+
+//Importamos el arhivo app.dart para utilizar
+import 'src/app.dart';
+
+//Esta es la funcion principal main()
+void main(){
+  //Esta es la funcion principal de la aplicacion
+  //El comando necesita un argumento, MyApp
+  runApp(MyApp());
+}
